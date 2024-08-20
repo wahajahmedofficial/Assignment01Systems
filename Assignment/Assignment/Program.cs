d5ba59f2e4424a9fb68eeca6f82d1acbc4711bb0
wahaj <wahaj.ahmed@codebtech.com>
2024-08-20 22:11:48 +0500

    assignment for 01 systems

---
+++ Assignment/Assignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+
+try
+{
+    string codeToAnalyze = File.ReadAllText(Path.Join(Environment.CurrentDirectory, "CodeToBeAnalyzed.txt"));
+    var compiledCode = CSharpScript.RunAsync(codeToAnalyze).Result;
+
+}
+catch (CompilationErrorException ex)
+{
+    Console.WriteLine($"Script compilation error: {string.Join(Environment.NewLine, ex.Diagnostics)}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Script execution error: {ex.Message}");
+}
+
+
